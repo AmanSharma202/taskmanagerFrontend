74fab4c11e0ad6f2196d3d15611090dec8fadb92
Aman Sharma <amansharma@Amans-MacBook-Air.local>
2026-04-06 00:39:25 +0530

    Change 1 frontend

---
+++ src/components/HeaderTopbar.jsx
@@ -0,0 +1,22 @@
+
+
+
+const HeaderTopbar = ()=>{
+    return <div className="header">
+        <div className="logo">
+          Task Manager
+        </div>
+
+        {/* Right Side - Navigation */}
+        <nav className="nav">
+          <a href="#" className="nav-link">All Tasks</a>
+          <a href="#" className="nav-link">Create</a>
+          <a href="#" className="nav-link">Calendar</a>
+          <a href="#" className="nav-link">About</a>
+        </nav>
+
+    
+    </div>
+}
+
+export default HeaderTopbar;
